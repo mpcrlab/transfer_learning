694aba44f3010dcd6ddd0cc2247e3bcdfd0fabca
Misha <mishakmak@gmail.com>
2018-06-30 12:34:29 -0400

    Changes to be committed: 	modified:   redneck_tensorboard.py now uses visdom

--- redneck_tensorboard.py
+++ redneck_tensorboard.py
@@ -1,45 +1,40 @@
-import matplotlib.pyplot as plt
 import time
 from sys import argv
-def redneck_tensorboard(run_id, xlim=None, fig=None):
+import visdom
+import argparse
+import numpy as np
+parser = argparse.ArgumentParser()
+parser.add_argument('-r','--run_id', help='Run Id of your makeshift log.', required=True)
+args = parser.parse_args()
+vis = visdom.Visdom()
+def redneck_tensorboard(run_id, windows, xlim=None):
     try:
-        if fig == None:
-            fi=plt.figure()
-            ax = fi.add_subplot(311)
-            ax.set_title('Training Loss')
-            bx = fi.add_subplot(313)
-            bx.set_title('Validation Loss')
-            cx = fi.add_subplot(311)
-            dx = fi.add_subplot(313)
-            fig = (fi, ax, bx, cx, dx)
+        win = [0,0,0]
         with open(str(run_id)+'.log') as f:
             contents = f.readlines()
         new = [contents[i:i+5] for i  in range(0, len(contents), 5)][:-1]
         val_loss = [float(i[3].split(' ')[2]) for i in new]
         train_loss = [float(i[2].split(' ')[2]) for i in new]
-        fig[1].set_title('Training Loss: {}'.format(train_loss[-1]))
-        fig[2].set_title('Validation Loss: {}'.format(val_loss[-1]))
-        fig[3].axhline(y=13)
-        fig[4].axhline(y=20)
+        title1 = ('Training Loss: {}'.format(train_loss[-1]))
+        title2 = ('Validation Loss: {}'.format(val_loss[-1]))
         if xlim == None:
-            fig[1].plot(train_loss)
-            fig[2].plot(val_loss)
+            pass
         elif type(xlim) != type(5):
             raise TypeError("expected xlim to be of type integer or None, got '{}'".format(xlim))
         else:
-            fig[1].plot(train_loss[:xlim])
-            fig[2].plot(val_loss[:xlim])
-        plt.pause(0.05)
-        plt.draw()
+            train_loss = train_loss[:xlim]
+            val_loss = val_loss[:xlim]
+        win[0] = vis.text('<h1>'+new[-1][0]+'</h1>', win=windows[0])
+        win[1] = vis.line(Y=np.array(train_loss), X=np.array(range(len(train_loss))), name=title1, win=windows[1], update='replace', opts=dict(fillarea=False,showlegend=False,width=800,height=800,xlabel='Epochs',ylabel='Loss',title=title2,marginleft=30, marginright=30,marginbottom=80,margintop=30,))
+        win[2] = vis.line(Y=np.array(val_loss), X=np.array(range(len(val_loss))), name=title2, win=windows[2], update='replace', opts=dict(fillarea=False,showlegend=False,width=800,height=800,xlabel='Epochs',ylabel='Loss',title=title1,marginleft=30, marginright=30,marginbottom=80,margintop=30,))
     except KeyboardInterrupt:
         quit()
-    return fig
+    return win
 try:
-    fig = redneck_tensorboard(argv[1])
+    r_id = args.run_id
+    fig = [vis.text('Initializing Training'),vis.line(np.array([0])),vis.line(np.array([0]))]
     while True:
-        print('going to display...')
         time.sleep(3)
-        fig = redneck_tensorboard(argv[1], fig=fig)
-        print('displayed.')
+        fig = redneck_tensorboard(r_id, fig)
 except KeyboardInterrupt:
     quit()
